47a7c529caad0d17afb29df26458f906c99d815d
Jon Nordby <jononor@gmail.com>
2017-04-22 03:03:54 +0200

    plugin: Remove delay code, just amp instead

--- plugin.cpp
+++ plugin.cpp
@@ -4,105 +4,49 @@
 #include <stdio.h>
 #include <array>
 
-/*****************************************************************************/
-
-#include "ladspa.h"
-
-/*****************************************************************************/
-
-/* The maximum delay valid for the delay line (in seconds). If you
-   change this, remember that the label is currently "delay_5s". */
-static const int MAX_DELAY = 5;
-
-/*****************************************************************************/
+#include <ladspa.h>
 
 /* The port numbers for the plugin: */
-
 enum PLUGIN_PORTS {
-    SDL_DELAY = 0,
-    SDL_DRY_WET,
-    SDL_INPUT,
-    SDL_OUTPUT,
+    PORT_VALUE = 0,
+    PORT_DRY_WET,
+    PORT_INPUT,
+    PORT_OUTPUT,
     PLUGIN_PORTS_N
 };
 
-/*****************************************************************************/
+#define CONSTRAIN(x, min, max) (((x) < min) ? min : (((x) > max) ? max : (x)))
 
-#define CONSTRAIN(x, min, max)          \
-(((x) < min) ? min : (((x) > max) ? max : (x)))
-
-/*****************************************************************************/
-
-/* Instance data for the simple delay line plugin. */
+/* Instance data */
 typedef struct {
-
   LADSPA_Data SampleRate;
-
-  // Internal buffer
-  LADSPA_Data * Buffer;
-
-  /* Buffer size, a power of two. */
-  unsigned long BufferSize;
-
-  /* Write pointer in buffer. */
-  unsigned long WritePointer;
-
-  // Ports
   LADSPA_Data *portData[PLUGIN_PORTS_N];
-
-} SimpleDelayLine;
-
-/*****************************************************************************/
+} InstanceData;
 
 /* Construct a new plugin instance. */
 static LADSPA_Handle 
 instantiate(const LADSPA_Descriptor * Descriptor, unsigned long SampleRate)
 {
-  SimpleDelayLine *self = (SimpleDelayLine *)malloc(sizeof(SimpleDelayLine));
+  InstanceData *self = new InstanceData;
   if (self == NULL) 
     return NULL;
   
   self->SampleRate = (LADSPA_Data)SampleRate;
 
-  /* Buffer size is a power of two bigger than max delay time. */
-  const unsigned long lMinimumBufferSize = (unsigned long)((LADSPA_Data)SampleRate * MAX_DELAY);
-  self->BufferSize = 1;
-  while (self->BufferSize < lMinimumBufferSize)
-    self->BufferSize <<= 1;
-  
-  self->Buffer = (LADSPA_Data *)calloc(self->BufferSize, sizeof(LADSPA_Data));
-  if (self->Buffer == NULL) {
-    free(self);
-    return NULL;
-  }
-
-  self->WritePointer = 0;
-
   return self;
 }
-
-/*****************************************************************************/
 
 /* Initialise and activate a plugin instance. */
 static void
 activate(LADSPA_Handle Instance) {
-
-  SimpleDelayLine * self;
-  self = (SimpleDelayLine *)Instance;
-
-  /* Need to reset the delay history in this function rather than
-     instantiate() in case deactivate() followed by activate() have
-     been called to reinitialise a delay line. */
-  memset(self->Buffer, 0, sizeof(LADSPA_Data) * self->BufferSize);
+  //InstanceData * self = (InstanceData *)Instance;
 }
-
-/*****************************************************************************/
 
 /* Connect a port to a data location. */
 static void 
 connectPort(LADSPA_Handle Instance, unsigned long Port, LADSPA_Data * DataLocation)
 {
-  SimpleDelayLine * self = (SimpleDelayLine *)Instance;
+  InstanceData * self = (InstanceData *)Instance;
   if (Port >= 0 and Port < PLUGIN_PORTS_N) {
     self->portData[Port] = DataLocation;
   } else {
@@ -110,43 +54,30 @@ connectPort(LADSPA_Handle Instance, unsigned long Port, LADSPA_Data * DataLocati
   }
 }
 
-/*****************************************************************************/
-
 /* Run a delay line instance for a block of SampleCount samples. */
 static void
 run(LADSPA_Handle Instance, unsigned long SampleCount) {
-  SimpleDelayLine * self = (SimpleDelayLine *)Instance;
+  InstanceData * self = (InstanceData *)Instance;
 
-  const unsigned long lDelay = CONSTRAIN(*(self->portData[SDL_DELAY]), 0, MAX_DELAY)*self->SampleRate;
-  const float fWet = CONSTRAIN(*(self->portData[SDL_DRY_WET]), 0, 1);
-  const float fDry = 1 - fWet;  
+  const float amp = CONSTRAIN(*(self->portData[PORT_VALUE]), 0, 1);
+  //const float fWet = CONSTRAIN(*(self->portData[PORT_DRY_WET]), 0, 1);
+  //const float fDry = 1 - fWet;  
 
-  LADSPA_Data *Input = self->portData[SDL_INPUT];
-  LADSPA_Data *Output = self->portData[SDL_OUTPUT];
+  LADSPA_Data *Input = self->portData[PORT_INPUT];
+  LADSPA_Data *Output = self->portData[PORT_OUTPUT];
 
-  const unsigned long lBufferWriteOffset = self->WritePointer;
-  const unsigned long lBufferReadOffset = lBufferWriteOffset + self->BufferSize - lDelay;
-  for (unsigned long lSampleIndex = 0; lSampleIndex < SampleCount; lSampleIndex++) {
-    LADSPA_Data fInputSample = *(Input++);
-    *(Output++) = (fDry * fInputSample
-		     + fWet * self->Buffer[((lSampleIndex + lBufferReadOffset) & (self->BufferSize-1))]);
-    self->Buffer[((lSampleIndex + lBufferWriteOffset) & (self->BufferSize-1))] = fInputSample;
+  for (unsigned long index = 0; index < SampleCount; index++) {
+    const LADSPA_Data in = *(Input++);
+    *(Output++) = (in * amp);
   }
-
-  self->WritePointer = ((self->WritePointer + SampleCount) & (self->BufferSize-1));
 }
 
-/*****************************************************************************/
-
-/* Throw away a simple delay line. */
+/* Cleanup instance data */
 static void
-cleanup(LADSPA_Handle Instance) {
-
-  SimpleDelayLine * self;
-  self = (SimpleDelayLine *)Instance;
-
-  free(self->Buffer);
-  free(self);
+cleanup(LADSPA_Handle Instance)
+{
+  InstanceData * self = (InstanceData *)Instance;
+  delete self;
 }
 
 struct Port {
@@ -180,11 +111,11 @@ struct Plugin {
 static Plugin plugin = {
     descriptor: {
         // metadata
-        UniqueID: 1043,
+        UniqueID: 1049,
         Label: "delay_5s",
         Properties: LADSPA_PROPERTY_HARD_RT_CAPABLE,
-        Name: "Simple Delay Line",
-        Maker: "Richard Furse (LADSPA example plugins)",
+        Name: "MicroFlo example plugin",
+        Maker: "Jon Nordby",
         Copyright: "None",
         PortCount: PLUGIN_PORTS_N,
         PortDescriptors: NULL,
@@ -206,7 +137,7 @@ static Plugin plugin = {
             {
                 HintDescriptor: LADSPA_HINT_BOUNDED_BELOW | LADSPA_HINT_BOUNDED_ABOVE,
                 LowerBound: 0.0,
-                UpperBound: (LADSPA_Data)MAX_DELAY,
+                UpperBound: 1.0,
             }
         },
         { "Dry/Wet Balance", LADSPA_PORT_INPUT | LADSPA_PORT_CONTROL,
