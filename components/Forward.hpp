ad175a2bcba744a362fa70d2d57f10779c176d57
Jon Nordby <jononor@gmail.com>
2017-05-14 00:28:20 +0200

    Forward: Only send on outport 0
    Shouldn't be possible with much else, but even so..

--- components/Forward.hpp
+++ components/Forward.hpp
@@ -14,7 +14,7 @@ class Forward : public SingleOutputComponent {
 public:
     virtual void process(Packet in, MicroFlo::PortId port) {
         if (in.isData()) {
-            send(in, port);
+            send(in);
         }
     }
 };
